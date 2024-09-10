2bb44added1bfef70c99edb3c809f675e30eb028
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2024-09-10 12:04:33 +0100

    Adding the tailored cv emailed

--- main.py
+++ main.py
@@ -2,8 +2,198 @@ import requests
 from fastapi import FastAPI
 from fastapi import FastAPI
 
-app = FastAPI()
+from urllib import parse
 
+import smtplib
+from os.path import basename
+from os import remove
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+APP_PASSWORD = "dcxc dhpf qhbt wxpr"
+def send_mail(send_from = "m.mohammadi.cie@gmail.com", send_to=["zumudcorporation@gmail.com"], subject="TestTailoredCV", text="This is a test email!", files=None, server="smtp.gmail.com", port=465):
+    assert isinstance(send_to, list)
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+
+    with smtplib.SMTP_SSL(server, port) as smtp_server:
+        smtp_server.login(user=send_from, password=APP_PASSWORD)
+        smtp_server.sendmail(send_from, send_to, msg.as_string())
+    
+
+app = FastAPI()
+latex_template = r"""\documentclass[11pt,a4paper,sans]{moderncv}
+
+\moderncvstyle{classic}
+\moderncvcolor{blue}
+
+\usepackage[scale=0.75]{geometry}
+\setlength{\footskip}{136.00005pt}
+
+\ifxetexorluatex
+  \usepackage{fontspec}
+  \usepackage{unicode-math}
+  \defaultfontfeatures{Ligatures=TeX}
+  \setmainfont{Latin Modern Roman}
+  \setsansfont{Latin Modern Sans}
+  \setmonofont{Latin Modern Mono}
+  \setmathfont{Latin Modern Math} 
+\else
+  \usepackage[T1]{fontenc}
+  \usepackage{lmodern}
+\fi
+
+\usepackage[english]{babel}
+
+\name{John}{Doe}
+\title{Résumé title}
+\born{4 July 1776}
+\address{street and number}{postcode city}{country}
+\phone[mobile]{+1~(234)~567~890}
+\phone[fixed]{+2~(345)~678~901}
+\phone[fax]{+3~(456)~789~012}
+\email{john@doe.org}
+\homepage{www.johndoe.com}
+
+\social[linkedin]{john.doe}
+\social[xing]{john\_doe}
+\social[twitter]{ji\_doe}
+\social[github]{jdoe}
+\social[gitlab]{jdoe}
+
+\extrainfo{additional information}
+\quote{Some quote}
+
+\renewcommand*{\bibliographyitemlabel}{[\arabic{enumiv}]}
+
+\begin{document}
+
+\makecvtitle
+
+\section{Education}
+\cventry{year--year}{Degree}{Institution}{City}{\textit{Grade}}{Description}
+\cventry{year--year}{Degree}{Institution}{City}{\textit{Grade}}{Description}
+
+\section{Master thesis}
+\cvitem{title}{\emph{Title}}
+\cvitem{supervisors}{Supervisors}
+\cvitem{description}{Short thesis abstract}
+
+\section{Experience}
+\subsection{Vocational}
+\cventry{year--year}{Job title}{Employer}{City}{}{General description no longer than 1--2 lines.\newline{}
+Detailed achievements:
+\begin{itemize}
+\item Achievement 1
+\item Achievement 2 (with sub-achievements)
+  \begin{itemize}
+  \item Sub-achievement (a);
+  \item Sub-achievement (b), with sub-sub-achievements;
+    \begin{itemize}
+    \item Sub-sub-achievement i;
+    \item Sub-sub-achievement ii;
+    \item Sub-sub-achievement iii;
+    \end{itemize}
+  \item Sub-achievement (c);
+  \end{itemize}
+\item Achievement 3
+\item Achievement 4
+\end{itemize}}
+\cventry{year--year}{Job title}{Employer}{City}{}{Description line 1\newline{}Description line 2\newline{}Description line 3}
+\subsection{Miscellaneous}
+\cventry{year--year}{Job title}{Employer}{City}{}{Description}
+
+\section{Languages}
+\cvitemwithcomment{Language 1}{Skill level}{Comment}
+\cvitemwithcomment{Language 2}{Skill level}{Comment}
+\cvitemwithcomment{Language 3}{Skill level}{Comment}
+\cvitemwithcomment{Language 4}{Skill level}{Comment}
+
+\section{Computer skills}
+\cvdoubleitem{category 1}{XXX, YYY, ZZZ}{category 4}{XXX, YYY, ZZZ}
+\cvdoubleitem{category 2}{XXX, YYY, ZZZ}{category 5}{XXX, YYY, ZZZ}
+\cvdoubleitem{category 3}{XXX, YYY, ZZZ}{category 6}{XXX, YYY, ZZZ}
+
+\section{Skill matrix}
+\cvitem{Skill matrix}{Alternatively, provide a skill matrix to show off your skills}
+
+\cvskilllegend*[1em]{}
+
+\cvskillhead[-0.1em]
+
+\cvskillentry*{Language:}{3}{Python}{2}{I'm so experienced in Python and have realised a million projects. At least.}
+\cvskillentry{}{2}{Lilypond}{14}{So much sheet music! Man, I'm the best!}
+\cvskillentry{}{3}{\LaTeX}{14}{Clearly I rock at \LaTeX}
+\cvskillentry*{OS:}{3}{Linux}{2}{I only use Archlinux btw}
+\cvskillentry*[1em]{Methods}{4}{SCRUM}{8}{SCRUM master for 5 years}
+
+\section{Interests}
+\cvitem{hobby 1}{Description}
+\cvitem{hobby 2}{Description}
+\cvitem{hobby 3}{Description}
+
+\section{Extra 1}
+\cvlistitem{Item 1}
+\cvlistitem{Item 2}
+\cvlistitem{Item 3. This item is particularly long and therefore normally spans over several lines. Did you notice the indentation when the line wraps?}
+
+\section{Extra 2}
+\cvlistdoubleitem{Item 1}{Item 4}
+\cvlistdoubleitem{Item 2}{Item 5\cite{book2}}
+\cvlistdoubleitem{Item 3}{Item 6. Like item 3 in the single column list before, this item is particularly long to wrap over several lines.}
+
+\section{References}
+\begin{cvcolumns}
+  \cvcolumn{Category 1}{\begin{itemize}\item Person 1\item Person 2\item Person 3\end{itemize}}
+  \cvcolumn{Category 2}{Amongst others:\begin{itemize}\item Person 1, and\item Person 2\end{itemize}(more upon request)}
+  \cvcolumn[0.5]{All the rest \& some more}{\textit{That} person, and \textbf{those} also (all available upon request).}
+\end{cvcolumns}
+
+\nocite{*}
+\bibliographystyle{plain}
+\bibliography{publications}
+
+\clearpage
+
+\recipient{Company Recruitment team}{Company, Inc.\\123 somestreet\\some city}
+\date{January 01, 1984}
+\opening{Dear Sir or Madam,}
+\closing{Yours faithfully,}
+\enclosure[Attached]{curriculum vit\ae{}}
+\makelettertitle
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ullamcorper neque sit amet lectus facilisis sed luctus nisl iaculis. Vivamus at neque arcu, sed tempor quam. Curabitur pharetra tincidunt tincidunt. Morbi volutpat feugiat mauris, quis tempor neque vehicula volutpat. Duis tristique justo vel massa fermentum accumsan. Mauris ante elit, feugiat vestibulum tempor eget, eleifend ac ipsum. Donec scelerisque lobortis ipsum eu vestibulum. Pellentesque vel massa at felis accumsan rhoncus.
+
+Suspendisse commodo, massa eu congue tincidunt, elit mauris pellentesque orci, cursus tempor odio nisl euismod augue. Aliquam adipiscing nibh ut odio sodales et pulvinar tortor laoreet. Mauris a accumsan ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse vulputate sem vehicula ipsum varius nec tempus dui dapibus. Phasellus et est urna, ut auctor erat. Sed tincidunt odio id odio aliquam mattis. Donec sapien nulla, feugiat eget adipiscing sit amet, lacinia ut dolor. Phasellus tincidunt, leo a fringilla consectetur, felis diam aliquam urna, vitae aliquet lectus orci nec velit. Vivamus dapibus varius blandit.
+
+Duis sit amet magna ante, at sodales diam. Aenean consectetur porta risus et sagittis. Ut interdum, enim varius pellentesque tincidunt, magna libero sodales tortor, ut fermentum nunc metus a ante. Vivamus odio leo, tincidunt eu luctus ut, sollicitudin sit amet metus. Nunc sed orci lectus. Ut sodales magna sed velit volutpat sit amet pulvinar diam venenatis.
+
+Albert Einstein discovered that $e=mc^2$ in 1905.
+
+\[ e=\lim_{n \to \infty} \left(1+\frac{1}{n}\right)^n \]
+
+\makeletterclosing
+
+\end{document}"""
 url = "https://api.openai.com/v1/chat/completions"
 headers = {
     "Content-Type": "application/json",
@@ -18,9 +208,31 @@ async def root():
 async def generator(resume_text: str, job_description_text: str):
     # Combine the two given URLs
     data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": f"Generate a cover letter for this job posting based on the resume content provided. Don't be overly formal. Keep it somewhat brief and speak about my particular experience and how it relates to the posting. Resume text: {resume_text} Job Description text: {job_description_text}"}],
+    "model": "gpt-3.5-turbo-0125",
+    "messages": [{"role": "user", "content": f"""Generate a tailored resume using for the given job description in the LaTeX template I give you. Latex template: {latex_template}.
+                  Resume text: {resume_text}.
+                  Job Description text: {job_description_text}"""}],
     "temperature": 0.7
 }
     response = requests.post(url, json=data, headers=headers)
-    return {"CoverLetter": response.json()}
+    ai_tailored_cv_response = response.json()["choices"][0]["message"]["content"]
+    tailored_cv_latex = ai_tailored_cv_response[ai_tailored_cv_response.find(r"\documentclass"):ai_tailored_cv_response.find(r"\end{document}")+len(r"\end{document}")]
+
+    print(f"Tailored CV latex code is:\n{tailored_cv_latex}")
+
+    latex_compiler_url = "https://latexonline.cc/compile?command=xelatex&text="
+    # git command example: https://texlive2020.latexonline.cc/compile?command=xelatex&git=https://github.com/mohammadi-com/resume&target=MohammadMohammadi2/MohammadMohammadi.tex
+
+    cv_pdf = requests.get(url=latex_compiler_url+parse.quote(tailored_cv_latex))
+
+    print(f"URL is: {cv_pdf.url}")
+    print(f"CV pdf text is: {cv_pdf.content}")
+
+    with open('./tailored_cv.pdf', 'wb') as f:
+        f.write(cv_pdf.content)
+    
+    send_mail(files=["./tailored_cv.pdf"])
+
+    remove("./tailored_cv.pdf")
+
+    return "sucess"
