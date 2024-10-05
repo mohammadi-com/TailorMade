546a2de3d28ba753c47e7833529e82acdb3fd018
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2024-10-05 08:56:39 +0100

    add json model to the responses

--- openai_wrapper.py
+++ openai_wrapper.py
@@ -1,14 +1,34 @@
+from pydantic import BaseModel
 from openai import OpenAI
 from envs import OPEN_AI_KEY
 from models import AIModel
 
 client = OpenAI(api_key=OPEN_AI_KEY)  # we recommend using python-dotenv to add OPENAI_API_KEY="My API Key" to your .env file so that your API Key is not stored in source control.
 
-def ai_prompt(prompt: str, model=AIModel.gpt_3_5_turbo_cheap):
+class TailoredApplication(BaseModel):
+    customized_resume: str
+    customized_cover_letter: str
+    company_name: str
+
+def ai_prompt(prompt: list, model=AIModel.gpt_3_5_turbo_cheap):
+    completion = client.chat.completions.create(
+        model=model,
+        messages=prompt,
+        response_format=TailoredApplication  # ensures the out put is a json with the given format
+        # [
+        #     {"role": "system", "content": "You are a helpful assistant."},
+        #     {"role": "user", 
+        #      "content": prompt}
+        # ]
+    )
+    return completion.choices[0].message.content
+
+
+def ai_prompt_str(prompt: str, model=AIModel.gpt_3_5_turbo_cheap):
     completion = client.chat.completions.create(
         model=model,
         messages=[
-            # {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", 
              "content": prompt}
         ]
