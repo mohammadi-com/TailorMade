3949b39bd591e5da847945936340c624c522d30a
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2024-10-05 18:26:46 +0100

    prompt engineering for better CV and cover letter resutls. Seperated prompts, and made specific instructions + Open AI structured output

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import requests
 from os import remove
 from fastapi import FastAPI
 from urllib import parse
+import json
 
 from envs import ADD_GDRIVE_ZAP_URL, LaTeX_COMPILER_URL
-from openai_wrapper import ai_prompt
+from openai_wrapper import create_customized_cv, create_customized_cl, ai_prompt
 from mail import send_mail
 from models import AIModel
 from cover_letter import save_string_to_pdf
 
-folder_num = 61  # maybe better to save it in a DB, so can keep it value when we restart the server
+folder_num = 90  # maybe better to save it in a DB, so can keep it value when we restart the server
 app = FastAPI()
 
 @app.get("/")
@@ -17,30 +18,27 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/generator")
-async def generator(resume_text: str, job_description_text: str, email_address: str | None = None, cover_letter: bool = False, model: AIModel | None = AIModel.gpt_3_5_turbo_cheap):
+async def generator(resume_text: str, job_description_text: str, email_address: str | None = None, cover_letter: bool = False, model: AIModel = AIModel.gpt_4o_mini):
     global folder_num
-    ai_tailored_cv_response = ai_prompt(f"""Generate tailored resume for the job post. Be specific, do not generalise, do not lie. My resume: {resume_text}. Job post: {job_description_text}""", model)
-    ai_tailored_cl_response = ai_prompt(f"""Generate a tailored cover letter using the given job description and my resume. At most two paragraphs. Focus on
-                                        my experiences and skills that align with the job description. Not overly formal. As the output, just give the cover letter, nothing else. Resume: {resume_text}. Job Description text: {job_description_text}""", model)
-    print(f"This is ai_tailored_cl_response: {ai_tailored_cl_response}")
-    job_name = ai_prompt(f"Give the name of the company that this job post is for. As the output hust give the name, nothing else. Job post: {job_description_text}")
-    tailored_cv_latex = ai_tailored_cv_response[ai_tailored_cv_response.find(r"\documentclass"):ai_tailored_cv_response.find(r"\end{document}")+len(r"\end{document}")]
-
+    ai_tailored_cv_response = create_customized_cv(resume_text, job_description_text, model)
+    ai_tailored_cl_response = create_customized_cl(resume_text, job_description_text, model)
+    company_name = ai_prompt(f"Give the name of the company that this job description is for. As the output hust give the name, nothing else. Job description: {job_description_text}")  # Since this is a simple task we use the cheapest ai    
+    
+    print(f"The ai_tailored_cv_response is: {ai_tailored_cv_response}\nThe ai_tailored_cl_response is: {ai_tailored_cl_response}\nthe company_name is: {company_name}")
+    tailored_cv_latex = ai_tailored_cv_response[ai_tailored_cv_response.find(r"\documentclass"):ai_tailored_cv_response.rfind(r"\end{document}")+len(r"\end{document}")]  # striming to the part we want
     print(f"Tailored CV latex code is:\n{tailored_cv_latex}")  # make these prints as logs
-
     latex_compiler_reponse = requests.get(url=LaTeX_COMPILER_URL+parse.quote(tailored_cv_latex))
     print(f"URL is: {latex_compiler_reponse.url}")
     print(f"CV pdf text is: {latex_compiler_reponse.content}")
-
     i = 1
-    while b"error: " in latex_compiler_reponse.content and i < 5:
-        ai_tailored_cv_response = ai_prompt(f"""Fix the latex code given with it's error. LaTeX: {tailored_cv_latex}. error: {latex_compiler_reponse.content}.""", model)
+    while b"error: " in latex_compiler_reponse.content and i < 5:  # if there is an error, provide whole previous response not the truncated
+        ai_tailored_cv_response = ai_prompt(f"""Fix the latex code given with it's error. LaTeX:\n{ai_tailored_cv_response}.\n\nError:\n{latex_compiler_reponse.content}.""", model)
         tailored_cv_latex = ai_tailored_cv_response[ai_tailored_cv_response.find(r"\documentclass"):ai_tailored_cv_response.find(r"\end{document}")+len(r"\end{document}")]
         print(f"{i} Corrected Tailored CV latex code is:\n{tailored_cv_latex}")
         latex_compiler_reponse = requests.get(url=LaTeX_COMPILER_URL+parse.quote(tailored_cv_latex))
         print(f"{i} Corrected URL is: {latex_compiler_reponse.url}")
         print(f"{i} Corrected CV pdf text is: {latex_compiler_reponse.content}")
-        i = i+1
+        i += 1
 
     
     tailored_cl_path = './tailored_cl.pdf'
@@ -54,7 +52,7 @@ async def generator(resume_text: str, job_description_text: str, email_address: 
 
     with open(tailored_cv_path, 'rb') as tailored_cv, open(tailored_cl_path, 'rb') as tailored_cl:
         files = {'tailored_cv': tailored_cv, 'tailored_cl': tailored_cl}
-        requests.post(url=ADD_GDRIVE_ZAP_URL, data={'name': f"{folder_num:04}_"+job_name}, files=files)
+        requests.post(url=ADD_GDRIVE_ZAP_URL, data={'name': f"{folder_num:04}_"+company_name}, files=files)
         folder_num += 1
     
     # we can use this line to send the cv to the email
--- openai_wrapper.py
+++ openai_wrapper.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel
 from openai import OpenAI
 from envs import OPEN_AI_KEY
@@ -5,32 +6,64 @@ from models import AIModel
 
 client = OpenAI(api_key=OPEN_AI_KEY)  # we recommend using python-dotenv to add OPENAI_API_KEY="My API Key" to your .env file so that your API Key is not stored in source control.
 
-class TailoredApplication(BaseModel):
+class TailoredCV(BaseModel):
     customized_resume: str
+
+class TailoredCL(BaseModel):
     customized_cover_letter: str
+
+class CompanyName(BaseModel):
     company_name: str
 
-def ai_prompt(prompt: list, model=AIModel.gpt_3_5_turbo_cheap):
-    completion = client.chat.completions.create(
+def create_customized_cv(resume_text: str, job_description_text: str, model=AIModel.gpt_4o_mini):
+    completion = client.beta.chat.completions.parse(
         model=model,
-        messages=prompt,
-        response_format=TailoredApplication  # ensures the out put is a json with the given format
-        # [
-        #     {"role": "system", "content": "You are a helpful assistant."},
-        #     {"role": "user", 
-        #      "content": prompt}
-        # ]
+        messages=[
+            {"role": "system", "content": """You will be given a job description. Create a customized resume in LaTeX.
+             Follow these instructions:
+             - change the title in a way that matches the job title
+             - Just focus on the contents. Do not change any settings, such as paper size, style, packages, and so on.
+             - For each past job in the experience section do not change the company name. Keep a 1-2 sentence about that company description. This should be concise but specific enough to give a good understanding of the past company.
+             - Make sure the skills in the job description are included in the resume.
+             - For items (bullet points) of each job rephrase them in a way that matches the language of the job description.
+             - Make technologies keywords bold. Bold syntax is \\textbf{text}.
+             - Run through all LaTeX after creation and make sure you're following LaTeX syntax.
+             - Make sure that each LaTeX instruction is in a single line. Don't put all the code in one line.
+             Resume LaTeX:""" + resume_text},
+            {"role": "user", "content": "Job description: "+job_description_text}
+        ],
+        response_format=TailoredCV  # ensures the out put is a json with the given format. For unsupported models, we can use JSON mode. read here: https://platform.openai.com/docs/guides/structured-outputs/json-mode
     )
-    return completion.choices[0].message.content
+
+    ai_tailored_cv_response, = json.loads(completion.choices[0].message.content).values()  # create the json object and unpack
+    return ai_tailored_cv_response
 
 
-def ai_prompt_str(prompt: str, model=AIModel.gpt_3_5_turbo_cheap):
+def create_customized_cl(resume_text: str, job_description_text: str, model=AIModel.gpt_4o_mini):
+    completion = client.beta.chat.completions.parse(
+        model=model,
+        messages=[
+            {"role": "system", "content": """You will be given a job description. Create a customized cover letter based on the resume is given.
+             Follow these instructions:
+             - The body of it should be at most two paragraphs.
+             - Focus on my experiences and skills that align with the job description.
+             - Not overly formal.
+             Resume:""" + resume_text},
+            {"role": "user", "content": "Job description: "+job_description_text}
+        ],
+        response_format=TailoredCL
+    )
+
+    ai_tailored_cl_response, = json.loads(completion.choices[0].message.content).values()  # create the json object and unpack
+    return ai_tailored_cl_response
+
+
+def ai_prompt(prompt: str, model=AIModel.gpt_3_5_turbo_cheap):
     completion = client.chat.completions.create(
         model=model,
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", 
-             "content": prompt}
+            {"role": "user", "content": prompt}
         ]
     )
     return completion.choices[0].message.content
