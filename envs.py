78afb0ac07d4646a7c9b6d434326c2ec6fc81efd
AliAfshar7 <ali_a_78@yahoo.com>
2024-12-15 11:41:35 +0000

    Add function to generate .tex and send .tar files
    We encountered a "414 URI Too Long" error when sending /text endpoint.
    Resolved it by generating .tex and .tar files and sending to the /data
    endpoint.

--- envs.py
+++ envs.py
@@ -7,5 +7,7 @@ load_dotenv()
 OPEN_AI_KEY = getenv("OPEN_AI_KEY")  # Put your open AI key here
 GMAIL_APP_PASSWORD = getenv("GMAIL_APP_PASSWORD")  # Put your gmail app password here
 ADD_GDRIVE_ZAP_URL = getenv("ADD_GDRIVE_ZAP_URL")  # Put zappier webhook workflow here, This webhook uploads the file to GDrive
-LaTeX_COMPILER_URL = "https://texlive2020.latexonline.cc/compile?command=pdflatex&text="
+LaTeX_COMPILER_URL_TEXT = "https://texlive2020.latexonline.cc/compile?command=pdflatex&text="
 # LaTeX_COMPILER_URL = "https://latexonline.cc/compile?command=xelatex&text="
+LaTeX_COMPILER_URL_DATA = "https://latexonline.cc/data?target=resume/resume.tex&force=true&command=pdflatex"
+
